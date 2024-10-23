144524aab6a6e09b79129c1930bbd55cd4ebde20
Levi-1103 <58707841+Levi-1103@users.noreply.github.com>
2024-10-23 01:24:59 +0100

    added chirps endpoint and creating chirps

--- handler_create_chirps.go
+++ handler_create_chirps.go
@@ -1,7 +1,90 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/Levi-1103/chirpy/internal/database"
+	"github.com/google/uuid"
+)
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
+}
 
 func (cfg *apiConfig) handlerCreateChirps(w http.ResponseWriter, r *http.Request) {
 
+	type parameters struct {
+		Body   string    `json:"body"`
+		UserId uuid.UUID `json:"user_id"`
+	}
+
+	type response struct {
+		Chirp
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	cleanedChirp, err := validateChirp(params.Body)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error(), err)
+	}
+
+	chirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
+		Body:   cleanedChirp,
+		UserID: params.UserId,
+	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create chirp", err)
+	}
+
+	respondWithJSON(w, http.StatusCreated, response{
+		Chirp: Chirp{
+			ID:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
+		},
+	})
+}
+
+func validateChirp(body string) (string, error) {
+	const maxChirpLength = 140
+	if len(body) > maxChirpLength {
+		return "", errors.New("Chirp is too long")
+	}
+	cleanedBody := getCleanedBody(body)
+
+	return cleanedBody, nil
+
+}
+
+func getCleanedBody(body string) string {
+
+	words := strings.Split(body, " ")
+
+	for i, word := range words {
+		wordLower := strings.ToLower(word)
+		if wordLower == "kerfuffle" || wordLower == "sharbert" || wordLower == "fornax" {
+			words[i] = "****"
+		}
+
+	}
+
+	cleanedBody := strings.Join(words, " ")
+	return cleanedBody
 }
